f4eff46398679492c3e743e3d602200447fd82d0
William Hinderman <williamhinderman@billhinraisembp.attlocal.net>
2016-06-21 00:18:38 -0500

    animation

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+$(function() {
+  $('.animate-in-1').removeClass('animate-in-1 animate-in');
+  var $a2 = $('.animate-in-2');
+  $a2.each(function(i) {
+      $(this).delay(1600+(400*i)).queue(function() {
+        $(this).removeClass('animate-in-2 animate-in')
+        .dequeue();
+      });
+  });
+  var $a3 = $('.animate-in-3');
+  $a3.each(function(j) {
+    $(this).delay(3200+(400*j)).queue(function() {
+      $(this).removeClass('animate-in-3 animate-in')
+      .dequeue();
+    });
+  });
+
+  var $a4 = $('.animate-in-4');
+  $a4.each(function(k) {
+    $(this).delay(4800+(400*k)).queue(function() {
+      $(this).removeClass('animate-in-4 animate-in')
+      .dequeue();
+    });
+  });
+
+});
